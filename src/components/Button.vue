aeeb61e2ddd5d3a9bf6cfa33a0f7bd74482272c7
KrisKringle1 <kmringler@gmail.com>
2021-03-05 16:31:02 -0800

    button

---
+++ src/components/Button.vue
@@ -0,0 +1,13 @@
+<template>
+    <button class="btn">{{text}}</button>
+</template>
+
+<script>
+export default {
+    name: "Button",
+    props: {
+        text: String,
+        color: String,
+    }
+}
+</script>
